796d41008f74b2d145d01d2f8bd53a012f23928d
DevNahyanK <nahyangaming789@gmail.com>
2025-01-22 00:01:51 +0500

    ✔️

--- sanity-migration/sanityClient.js
+++ sanity-migration/sanityClient.js
@@ -9,9 +9,9 @@ const client_1 = require("@sanity/client");
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 exports.client = (0, client_1.createClient)({
-    projectId: process.env.projectId, // Replace with your project ID
+    projectId: 't9ags2vo', // Replace with your project ID
     dataset: 'production', // Or your dataset name
     apiVersion: '2024-01-04', // Today's date or latest API version
     useCdn: false, // Disable CDN for real-time updates
-    token: process.env.token,
+    token: 'skJ3GyGn9pUdpceqUy9kU0ULzNbeyGwyRmB3eyWYWWXOpqhqmmtFVa5YGae87vtLReSRu2RjKc5bJyKJjUBgR1vCzMwf91MemWZqm2y7WtgW6LLyj5XWKtg9EazHLFlg0LkcYtxai8sJq79NKEwfS9gFG9KLfGd9gdHXAAOn0SocStcR3njy',
 });
